ea81f59dbd5e4605b96984cf4a8006666c15afdf
rathberm <moritzrathberger@gmail.com>
2019-09-06 07:44:48 +0200

    added error messages for 404 and 500 added mailserver

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -12,4 +13,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
